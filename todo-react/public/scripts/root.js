ebcafbf81e68e4fb1cd1011b21413fd34d31bfb4
akinyiv <akinyivee10@gmail.com>
2022-08-02 12:41:37 +0300

    babel

--- todo-react/public/scripts/root.js
+++ todo-react/public/scripts/root.js
@@ -1,13 +1,43 @@
 "use strict";
 
 // JSX - javascript xml
+var app = {
+    title: "Todo Decisions",
+    subtitle: "things to focus on",
+    options: ["baseball", "soccer", "tennis", "golf"]
+};
+
 var template = React.createElement(
     "div",
     null,
     React.createElement(
         "h1",
         null,
-        "Todo App"
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.options.length > 0 ? "here are your options" : "no options here"
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Nairobi"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Malindi"
+        )
     )
 );
 
--- todo-react/src/app.js
+++ todo-react/src/app.js
@@ -1,7 +1,19 @@
 // JSX - javascript xml
+let app = {
+    title: "Todo Decisions",
+    subtitle: "things to focus on",
+    options: ["baseball", "soccer", "tennis", "golf"],
+}
+
 let template = (
     <div>
-        <h1>Todo App</h1>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "here are your options" : "no options here"}</p>
+        <ul>
+            <li>Nairobi</li>
+            <li>Malindi</li>
+        </ul>
     </div>
 );
 
